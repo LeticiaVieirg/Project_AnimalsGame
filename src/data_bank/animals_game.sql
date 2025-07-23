1cd685485827b01d4db759f5b2fe8c891a614508
Letícia Vieira Gonçalves <90807534+LeticiaVieirg@users.noreply.github.com>
2025-07-22 21:37:41 -0300

    Update animals_game.sql

--- src/data_bank/animals_game.sql
+++ src/data_bank/animals_game.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE bdjb;
-USE bdjb;
+drop database animals_game;
+create database if not exists animals_game;
+use animals_game;
 
 CREATE TABLE cliente(
 	id_cliente INT AUTO_INCREMENT PRIMARY KEY,
@@ -19,8 +20,6 @@ CREATE TABLE extracao(
     data_extracao DATETIME
 );
 
-
-
 CREATE TABLE aposta(
 	id_aposta INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT,
@@ -31,6 +30,7 @@ CREATE TABLE aposta(
     FOREIGN KEY (id_extracao) REFERENCES extracao (id_extracao),
     FOREIGN KEY (id_animal) REFERENCES animal (id_animal)
 );
+
 CREATE TABLE resultado(
 	id_resultado INT AUTO_INCREMENT PRIMARY KEY,
     id_extracao INT,
@@ -39,9 +39,7 @@ CREATE TABLE resultado(
     FOREIGN KEY (id_animal) REFERENCES animal (id_animal)
 );
 
-
--- Povoando Banco de Dados
-
+-- Povoamento do Banco de Dados
 INSERT INTO cliente(nome_cliente, telefone, email, saldo) VALUES
 ('João Pedro', '84988998899', 'joao.pedro@email.com', 230.00),
 ('Ana Clara', '83997989798', 'ana.clara@email.com', 200.00),
@@ -49,33 +47,13 @@ INSERT INTO cliente(nome_cliente, telefone, email, saldo) VALUES
 ('Diego Rafael', '84967879678', 'diego.rafael@email.com', 180.00),
 ('Mateus Henrique', '81983458769', 'mateus.henrique@email.com', 140.00);
 
-
-
 INSERT INTO animal (id_animal, nome_animal) VALUES
-(1, 'Avestruz'),
-(2, 'Aguia'),
-(3, 'Burro'),
-(4, 'Borboleta'),
-(5, 'Cachorro'),
-(6, 'Cabra'),
-(7, 'Carneiro'),
-(8, 'Camelo'),
-(9, 'Cobra'),
-(10, 'Coelho'),
-(11, 'Cavalo'),
-(12, 'Elefante'),
-(13, 'Galo'),
-(14, 'Gato'),
-(15, 'Jacaré'),
-(16, 'Leão'),
-(17, 'Macaco'),
-(18, 'Porco'),
-(19, 'Pavão'),
-(20, 'Peru'),
-(21, 'Touro'), 
-(22, 'Tigre'),
-(23, 'Urso'),
-(24, 'Veado'),
+(1, 'Avestruz'), (2, 'Aguia'), (3, 'Burro'), (4, 'Borboleta'),
+(5, 'Cachorro'),(6, 'Cabra'),(7, 'Carneiro'),(8, 'Camelo'),
+(9, 'Cobra'),(10, 'Coelho'),(11, 'Cavalo'),(12, 'Elefante'),
+(13, 'Galo'),(14, 'Gato'),(15, 'Jacaré'),(16, 'Leão'),
+(17, 'Macaco'),(18, 'Porco'),(19, 'Pavão'), (20, 'Peru'),
+(21, 'Touro'), (22, 'Tigre'),(23, 'Urso'),(24, 'Veado'),
 (25, 'Vaca');
 
 INSERT INTO extracao(id_extracao, data_extracao) VALUES
@@ -101,20 +79,17 @@ INSERT INTO resultado (id_extracao, id_animal) VALUES
 
 
 SELECT 
-		a.id_aposta,
-		c.nome_cliente,
-		e.data_extracao,
-		an.nome_animal AS animal_apostado,
-		a.valor_apostado,
-		a.valor_apostado * 18.00 AS valor_premio
-    
-FROM 
-		aposta a
-    
+	a.id_aposta,
+	c.nome_cliente,
+	e.data_extracao,
+	an.nome_animal AS animal_apostado,
+	a.valor_apostado,
+	a.valor_apostado * 18.00 AS valor_premio
+FROM
+	aposta a
 JOIN cliente c ON a.id_cliente = c.id_cliente
 JOIN extracao e ON a.id_extracao = e.id_extracao 
 JOIN animal an ON a.id_animal = an.id_animal
 JOIN resultado r ON a.id_extracao = r.id_extracao
+WHERE a.id_animal = r.id_animal;
 
-WHERE 
-		a.id_animal = r.id_animal;
